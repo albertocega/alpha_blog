45782565c942b28a75df5731796bca453d39507a
albertocega <albertocelgal@gmail.com>
2017-09-09 00:06:11 +0000

    after user new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,8 @@ class ArticlesController < ApplicationController
 
         @article = Article.new(article_params)
         
+        @article.user = User.first
+        
         if @article.save
             
             flash[:success] = "Article succesfully created"
